2b4b4391018791a9386ea700bddded9bab17b677
Ongeri123 <ongerinewton10@gmail.com>
2025-06-28 19:53:04 +0300

    Building project structure

---
+++ index.js
@@ -0,0 +1,7 @@
+let searchInput = document.getElementById("site-search");
+
+let authorCheck = document.getElementById("author");
+
+let subjectCheck = document.getElementById("subject");
+
+let searchTextLine = document.getElementById("search-text-line");
